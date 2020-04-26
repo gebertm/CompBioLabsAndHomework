8f5864898c55d5116e95be4bb9009faaa3a8c2be
gebertm <matthew.gebert@colorado.edu>
2020-04-26 16:49:21 -0600

    Rough draft of independent project: R Code + markdown file

---
+++ IndependentProject_Biofilm_pH/EBIO5420_CompBio_IndependentProject_README.md
@@ -0,0 +1,107 @@
+# EBIO 5420: Computational Biology
+
+# The Effect of Environmental pH on Different Bacterial Phyla Residing in Showerhead Biofilms 
+
+## The main question to be addressed using these data is: _"how does the pH of water in premise plumbing
+## systems effect the relative abundance of different biofilm-residing bacterial taxa at the phylum level."_
+
+## *Ecological Background:* _pH dependencies, biofilms, and the Showerhead Microbiome Project_
+ph of the surrounding environment, be it in the environment or in culture, is an important determinant of where microbes live (Lauber et al, 2008, 1).
+Some bacteria prefer a low pH environment, while others prefer a more neutral, or even highly basic environment. pH also impacts 
+the metabolism of microbial cells, as well as cell physiology. Understanding how environmental conditions, like pH, predict the abundance of a
+certain bacterial phylum is an important step in being able to predict what bacteria live where, especially when considering potential 
+pathogens. 
+	Municipal water systems are known to harbor many types of bacteria, and pH of these systems varies based on source of water and geographic 
+location (Gebert et al, 2018, 2). Generally, these bacteria live in complex communities within an extracellular matrix composed of "extracellular polymeric substances" (ESP),
+secreted by the bacteria. Given the importance of pH in determining where some microbes can be found, understanding how biofilm bacterial communities
+are effected by pH can be an important step in determining what types of bacteria reside in municipal water systems, and potentially help to better understand how
+pH effects the bacteria across many different biomes. Understanding how environmental factors shape bacterial communities can also be an important component 
+of disease epidemiology. Predicting the presence of certain bacterial taxa based on environmental traits can help to better predict potential areas of exposure, and
+routes of transmission. 
+
+## *Showerhead Microbiome Project: Background*
+The overall goal of the original study was to determine what factors (geographic location, water chemistry, water source, etc) influence the 
+relative abundance and biogeographic variation in the genus _Mycobacterium_. The study set out to collect biofilms from municipal and well-water fed 
+showerheads across the United States and Europe. Since genus _Mycobacterium_ contains potential pathogens, understanding what contributes to their
+presence and survival in showerhead biofilms is an important first step in better understanding the epidemiology of disease.
+
+Volunteer participants across the US and Europe contributed a swab from their household showerhead, which was then sent to CU Boulder for bacterial analysis
+using high-throughput sequencing technology. Along with the swab, participants also provided geographic location, basic water chemistry using a dipstrip (for example, 
+pH, chlorine, iron concentration) water source, and basic information about their showerheads (material, spray pattern). Metadata was compiled for each participant 
+and compared to the relative abundance of genus _Mycobacterium_ across the sample set, as well as the relative abundance of specific mycobacterial species to determine
+if different species (especially pathogens) were more abundant in different parts of the US in Europe.
+
+The study looked to determine the presence and relative abundance of genus _Mycobacterium_ in the biofilm samples, and to determine specifically which species 
+of mycobacteria were present in the sample. Using the metadata provided by the participants, researchers looked for biogeographic patterns and predictors of presence
+and relative abundance of both innocuous and pathogenic mycobacteria.
+
+
+## Methods
+
+The data for the study was a result of a 2018 publication titled [*'Ecological analyses of Mycobacteria in showerhead biofilms and their relevance to human health'*](https://mbio.asm.org/content/9/5/e01614-18.abstract)
+
+The 16S rRNA data were produced by a high-throughput sequencing effort of 700+ biofilm swabs from premise plumbing biofilm swabs from across the United States and Europe.
+The process consisted of recruiting volunteer participants to collect biofilm from their showerheads, sending it back to the University of Colorado, and subsequently, extracting and
+sequencing the bacterial DNA on the swab. The data was then processed using the USearch pipeline (Edgar) to pick operational taxonomic units (OTUs) and to classify the OTUs using
+a reference database (Greengenes 16S database in this case). 
+The resulting information, after sequencing and data processing, was a table of all the bacterial taxa that were present in the biofilm sample 
+collected on the swab (a proxy for the biofilm that was collecting in the showerhead as a whole, as well as in the premise plumbing). 
+	Also collected, using inexpensive test strips, was basic water chemistry data, which included a rough estimate of pH. 
+ 
+The initial data itself consisted of 2 Excel files - one OTU table (which consisted of sampleID with corresponding read counts and taxonomic identification), and one metadata file, populated with a tremendous amount
+of information regarding water chemistry, as well as geographic location, and basic information about the showerhead itself (material, use frequency, etc). Neither file was particularly large (given it was the output
+of the raw sequence data, which was much larger). The metadata file was 151KB, while the OTU table was 7.6 MB. Some of the issues that were initially noted was small inconsistencies in data reporting
+
+## Approach
+
+In the original analyses, the focus was primarily on the potential respiratory pathogen, mycobacteria. Relative abundance of genus Mycobacterium was determined by comparing number of reads of mycobacteria to the
+number of total reads per sample (# of mycobacterial reads for sample x / # of total reads for sample x). This allows for one to determine how much of the biofilm swab sample bacterial community was made up
+of genus Mycobacterium. The relative abundance of genus Mycobacterium was then compared to the collected water chemistry data to look for patterns or correlations, for example, with levels of chlorine. 
+
+The data was originally stitched together using Excel. Figures were made either in R or using a propriety statistical analysis package. 
+
+My approach and analysis was done entirely in R. Starting with the raw data (a complete OTU table in which samples with less than 2000 reads had already been removed, and a complete metadata file), and
+in order to create a fully reproducible work flow, I began my analyses by choosing three bacterial phyla (Bacteroidetes, Cyanobacteria, and Chloroflexi) in order to further investigate how pH effected each
+taxa individually. This involved first splitting the 'taxonomy' column into the individual components of the Linnaean classifaction system, so that sorting by individual phylum would be much easier. It was also important
+to remove any samples initially that did not need to be included in the analysis (for example, clean control samples, as well as samples that were not from biofilms).	
+
+Next, it was important to begin to remove any inconsistencies and missing data from the dataset, to prevent any downstream issues. For example, this involved removing NAs and any holes from the table. 
+
+Given the sheer volume of the dataset, it was important to parse the OTU table so that just the 3 phyla of interest were represented, with all the samples of interest included. This condensing a very large table into
+one that is only the read counts for each sample where the phyla are present. With a condensed table, the next two steps were calculating total read counts per sample for each of the taxa in each sample, as well as the
+total read counts for each sample. This allows for the calculation of relative abundance for each individual phylum. 
+
+
+The final step, after cleaning up the table, parsing out the phyla that were not of interest, and calculating relative abundance for each of the phyla per sample, was to combine the pH readings from the metadata, which
+involved combining to dataframes. 
+
+The result of the code is an abridged table, with all relavent samples present, parsed by phyla, with corresponding taxon of interest reads per sample, total reads per sample, and relative abundance (%). This allowed 
+for two graphs to be made for each of the taxa of interest, as well as statistical tests to be run to determine how each bacterial phylum responded to the pH of water in a premise plumbing system. 
+
+
+## Results and Concluding Statements
+
+Looking at both the graphical representations of the data, as well as the output from the linear regression, it appears as though there is no strong correlation between the pH of the water in the premise plumbing 
+system and the relative abundance of each of the bacterial taxa. This does not necessarily mean that pH is not having an effect on bacterial relative abundance for Bacteroidetes, Cyanobacteria, and Chloroflexi, but 
+rather it means that there is not a strong enough correlation to say anything definitive regarding the response to this environmental parameter. This helps to potentially address, albeit on a surface level, my question
+of how does pH impact the relative abundance of bacteria in biofilms. 
+
+This is not surprising for 2 reasons, 1) because it is very difficult to disentangle the effects of multiple environmental parameters on biofilm residing bacterial communities, and 2) it pH likely does not impact 
+bacterial taxa in a linear fashion, given that each taxa can have a range of tolerances.
+
+
+
+#### _References:_
+1. Lauber, C.L., Strickland, M.S., Bradford, M.A., Fierer, Noah The influence of soil properties on the structure of bacterial and fungal communities across land-use types. Soil Biology and Biochemistry, 2008. 40(9): p. 2407 - 2415.
+[https://doi.org/10.1016/j.soilbio.2008.05.021](https://doi.org/10.1016/j.soilbio.2008.05.021)
+
+2. Gebert MJ, Delgado-Baquerizo M, Oliverio AM, Webster TM, Nichols LM, Honda JR, Chan ED, Adjemian J, Dunn RR, Fierer N. 2018. Ecological analyses of mycobacteria in showerhead biofilms and their relevance to human health. 
+mBio 9:e01614-18. [https://doi.org/10.1128/mBio.01614-18](https://doi.org/10.1128/mBio.01614-18)
+
+
+
+
+
+
+
+
---
+++ IndependentProject_Biofilm_pH/Gebert_FinalProject_Biofilm_pH_RCode_03282020.R
@@ -0,0 +1,177 @@
+### The Showerhead Microbiome Project - 16S rRNA Data
+### Determine if relative abundance of 1. Cyanobacteria, 2. Bacteroidetes, and 3. Chloroflexi are impacted 
+### by the pH of the water system
+### MiSeq, 2 X 150
+### Manuscript and Dataset: Gebert et al, 2018
+
+# Question: Does pH impact the relative abundance of Cyanobacteria in municipal biofilms
+
+# reading in necessary libraries
+library(ggplot2)
+library(plyr)
+library(plotly)
+library(tidyverse)
+library(tidyr)
+
+# Reading in OTU table --- Full dataset as a .csv
+FullOTUTable <- read.csv('~/Desktop/Spring Semester - 2020 Classwork/EBIO5420 Computational Biology/Showerhead_Microbiome_Full_Combined_otu_table_wTax_noChloroMito_June2017_2000+_Reads.csv', header = TRUE, strip.white = TRUE)
+
+# Reading in full metadata file as a .csv - metadata has been anonymized
+FullMetadata <- read.csv('~/Desktop/Spring Semester - 2020 Classwork/EBIO5420 Computational Biology/Showerhead_Microbiome_Full_Metadata_Anonymized_03282020.csv', header = TRUE) 
+
+# Saving the full OTU table (all taxa) as a data frame
+FullOTUTable <- data.frame(FullOTUTable)
+
+# Summing all read counts for each row to generate a column of total reads per each taxonomic identifier
+# This will condense the enormous table down to one column - easier to complete before splitting taxonomy into 
+# unique columns. 
+TotalCountsPerOTU <- within(FullOTUTable, rm('X.OTU.ID', 'X', 'taxonomy')) # Removing headers without numerical data
+TotalCountsPerOTU_Clean <- TotalCountsPerOTU %>% select(-contains("NTC")) %>% select(-contains("Dust")) %>% select(-contains("EXT")) # Removing extraneous samples 
+TotalCountsPerOTU_Clean$ReadSum <- rowSums(TotalCountsPerOTU_Clean) # summing all the reads in the rows, saved as a new column
+ReadSum_Rows <- as.data.frame(TotalCountsPerOTU_Clean$ReadSum)
+
+# Splitting taxonomic IDs into separate columns for ease using Tidyr - currently all separated by ; in one column
+# https://tidyr.tidyverse.org/reference/separate.html
+FullOTU_SplitTaxa <- FullOTUTable %>% separate(taxonomy, c('Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species'), sep = ";")
+
+# Adding read sum column on to original OTU table (data frame) so that total viable read counts are included
+# This will be used to calculate total relative abundance down the line
+FullOTU_SplitTaxa <- FullOTU_SplitTaxa %>% add_column(ReadSum_Rows)
+
+# Removing errant 'X' column -> miscalculated total read numbers
+FullOTU_SplitTaxa <- within(FullOTU_SplitTaxa, rm('X'))
+
+# Renaming OTU header to remove leading X.
+FullOTU_SplitTaxa <- FullOTU_SplitTaxa %>% rename(OTU_ID = X.OTU.ID)
+
+# Checking for presence of NA values in Phylum column
+is.na(FullOTU_SplitTaxa)
+
+# Remove NA rows from dataset - potentially causing problems with subsetting function
+FullOTU_SplitTaxa_Clean <- na.omit(FullOTU_SplitTaxa) # removing _all_ NAs from the dataset
+
+# Removing Extraction blanks, control samples, and errant dust samples
+FullOTU_SplitTaxa_Clean <- FullOTU_SplitTaxa_Clean %>% select(-contains("NTC")) %>% select(-contains("Dust")) %>% select(-contains("EXT")) # Removing extraneous samples 
+
+# Getting column sums for entire taxa table
+AllColumnSum <-FullOTU_SplitTaxa_Clean %>% summarize_if(is.numeric, sum)
+
+# Keep rows where Phylum column is equal to 'Cyanobacteria', 'Bacteroidetes', or 'Chloroflexi'
+CyanoRowsOnly <- FullOTU_SplitTaxa_Clean[FullOTU_SplitTaxa_Clean$Phylum == " p__Cyanobacteria", ]
+BactRowsOnly <- FullOTU_SplitTaxa_Clean[FullOTU_SplitTaxa_Clean$Phylum == " p__Bacteroidetes", ]
+ChloroRowsOnly <- FullOTU_SplitTaxa_Clean[FullOTU_SplitTaxa_Clean$Phylum == " p__Chloroflexi", ]
+
+# Summing all the OTU reads in each of the samples - Counts of Cyanobacteria, Bacteroidetes, Chloroflexi in each sample
+Cyano_rowSum <- CyanoRowsOnly %>% summarize_if(is.numeric, sum)
+Bact_rowSum <- BactRowsOnly %>% summarize_if(is.numeric, sum)
+Chloro_rowSum <- ChloroRowsOnly %>% summarize_if(is.numeric, sum) 
+
+# Creating a dataframe consisting of SampleID, sum of total reads per sample, and sum of total reads per sample for each
+# of the bacterial phyla
+Cyano_Merged <- rbind(Cyano_rowSum, AllColumnSum)
+Bact_Merged <- rbind(Bact_rowSum, AllColumnSum)
+Chloro_Merged <- rbind(Chloro_rowSum, AllColumnSum)
+
+# Transposing the dataframe - will make downstream analysis easier
+CyanoMerged_Transposed <- as.data.frame(t(Cyano_Merged))
+BactMerged_Transposed <- as.data.frame(t(Bact_Merged))
+ChloroMerged_Transposed <- as.data.frame(t(Chloro_Merged))
+
+# Renaming, Column 0, V1 and V2 headers in data frame to reflect actual data - bacterial read counts per sample and total read counts per sample
+Cyano_RelAbundTable <- tibble::rownames_to_column(CyanoMerged_Transposed, "ID.Number")
+Cyano_RelAbundTable <- Cyano_RelAbundTable %>% rename(CyanoReadCounts = V1, TotalReadCounts = V2) 
+Bact_RelAbundTable <- tibble::rownames_to_column(BactMerged_Transposed, "ID.Number")
+Bact_RelAbundTable <- Bact_RelAbundTable %>% rename(BacteroidetesReadCounts = V1, TotalReadCounts = V2)
+Chloro_RelAbundTable <- tibble::rownames_to_column(ChloroMerged_Transposed, "ID.Number")
+Chloro_RelAbundTable <- Chloro_RelAbundTable %>% rename(ChloroflexiReadCounts = V1, TotalReadCounts = V2)
+
+# Calculating relative abundance of Cyanobacteria/Bacteroidetes/Chloroflexi in the dataset - Relative abundance = (phylum of interest total reads per sample / total reads per sample) * 100
+Cyano_RelAbundTable$CyanoRelativeAbundance <- ((Cyano_RelAbundTable$CyanoReadCounts / Cyano_RelAbundTable$TotalReadCounts) * 100)
+Bact_RelAbundTable$BacterRelativeAbundance <- ((Bact_RelAbundTable$BacteroidetesReadCounts / Bact_RelAbundTable$TotalReadCounts) * 100)
+Chloro_RelAbundTable$ChloroRelativeAbundance <- ((Chloro_RelAbundTable$ChloroflexiReadCounts / Chloro_RelAbundTable$TotalReadCounts) * 100)
+
+# Combining all three phyla into one table for downstream analysis
+FullRelAbundTable <- bind_cols(Cyano_RelAbundTable, Bact_RelAbundTable, Chloro_RelAbundTable)
+FullRelAbundTable <- subset(FullRelAbundTable, select = -c(ID.Number1, ID.Number2, TotalReadCounts1, TotalReadCounts2))
+##### Now dealing with the metadata file (creating a dataframe of sampleID and pH value) #####
+
+# Making a dataframe of only SampleID and pH of water for each sample 
+# Cleaning up data frame
+FullMetadata <- as.data.frame(FullMetadata)
+pH_data_Full <- FullMetadata %>% select(ID.Number, pH) # Selecting SampleID and pH columns - creating new dataframe
+
+# Removing NAs and Non-integers ('>' characters)
+pH_data_Full_clean <- pH_data_Full %>% na_if("") %>% na.omit
+
+# Combining two data frames - generating 1 dataframe with SampleID, Cyano Relative Abundance, and pH value
+# Potentially going to need to remove special characters, although pH does not need to be continuous
+DataTable_PhylaRelAbund_pH <- FullRelAbundTable %>% inner_join(pH_data_Full_clean)
+
+# Removing rows where pH is < 6.2 - very broad and not terribly informative for this study
+DataTable_PhylaRelAbund_pH <- subset(DataTable_PhylaRelAbund_pH, DataTable_PhylaRelAbund_pH$pH != "< 6.2")
+
+# Figure 1: Boxplot representing the 3 bacterial phyla and their average relative abundance
+library(cowplot)
+# Boxplots
+pH_Plot_Bacter <- ggplot(data = DataTable_PhylaRelAbund_pH, aes(x = pH, y = BacterRelativeAbundance )) +
+  geom_boxplot(color="black", fill="green", alpha=0.9) + labs(x = "pH of Water Source", y = "Relative Abundance of Bacteroidetes (%)") + 
+  theme(axis.text.x = element_text(angle = 70, hjust = 1))
+pH_Plot_Cyano <- ggplot(data = DataTable_PhylaRelAbund_pH, aes(x = pH, y = CyanoRelativeAbundance )) +
+  geom_boxplot(color="black", fill="yellow", alpha=0.9) + labs(x = "pH of Water Source", y = "Relative Abundance of Cyanobacteria (%)") +
+  theme(axis.text.x = element_text(angle = 70, hjust = 1))
+pH_Plot_Chloro <- ggplot(data = DataTable_PhylaRelAbund_pH, aes(x = pH, y = ChloroRelativeAbundance )) +
+  geom_boxplot(color="black", fill="orange", alpha=0.9) + labs(x = "pH of Water Source", y = "Relative Abundance of Chloroplexi (%)") +
+  theme(axis.text.x = element_text(angle = 70, hjust = 1))
+
+# Scatterplots 
+pH_Plot_Bacter_scatter <- ggplot(data = DataTable_PhylaRelAbund_pH, aes(x = pH, y = BacterRelativeAbundance )) +
+  geom_jitter(shape=21, fill='plum', size=1.5) + labs(x = "pH of Water Source", y = "Relative Abundance of Bacteroidetes (%)") +
+  theme(axis.line=element_line(size=1, color='black'),
+        text=element_text(family = 'Times', face='bold', size=8),
+        panel.grid.major = element_line(color='grey'),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(), panel.background = element_blank(),
+        axis.text.x=element_text(color='black', size=8, face='bold', angle = 70, hjust = 1),
+        axis.text.y=element_text(color='black', size=8, face='bold'))
+pH_Plot_Cyano_scatter <- ggplot(data = DataTable_PhylaRelAbund_pH, aes(x = pH, y = CyanoRelativeAbundance )) +
+  geom_jitter(shape=21, fill='red', size=1.5) + labs(x = "pH of Water Source", y = "Relative Abundance of Cyanobacteria (%)") +
+  theme(axis.line=element_line(size=1, color='black'),
+        text=element_text(family = 'Times', face='bold', size=8),
+        panel.grid.major = element_line(color='grey'),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(), panel.background = element_blank(),
+        axis.text.x=element_text(color='black', size=8, face='bold', angle = 70, hjust = 1),
+        axis.text.y=element_text(color='black', size=8, face='bold'))
+pH_Plot_Chloro_scatter <- ggplot(data = DataTable_PhylaRelAbund_pH, aes(x = pH, y = ChloroRelativeAbundance )) +
+  geom_jitter(shape=21, fill='orange', size=1.5) + labs(x = "pH of Water Source", y = "Relative Abundance of Chloroplexi (%)") +
+  theme(axis.line=element_line(size=1, color='black'),
+        text=element_text(family = 'Times', face='bold', size=8),
+        panel.grid.major = element_line(color='grey'),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(), panel.background = element_blank(),
+        axis.text.x=element_text(color='black', size=8, face='bold', angle = 70, hjust = 1),
+        axis.text.y=element_text(color='black', size=8, face='bold'))
+
+
+# Using cowplot to create one figure - easier to compare side by side
+plot_grid(pH_Plot_Bacter, pH_Plot_Chloro, pH_Plot_Cyano, nrow = 1, labels = c('A', 'B', 'C'))
+
+firstPlot <- pH_Plot_Bacter_scatter
+secondPlot <- pH_Plot_Cyano_scatter
+thirdPlot <- pH_Plot_Chloro_scatter
+
+fullPlot <- plot_grid(firstPlot, secondPlot, thirdPlot, labels=c('', ''), ncol=1)
+fullPlot
+
+# Does pH effect relative abundance of Cyanobacteria?
+# Spearman Correlation 
+model_bact <- lm(DataTable_PhylaRelAbund_pH$BacterRelativeAbundance ~ DataTable_PhylaRelAbund_pH$pH)
+print(summary(model_bact))  
+
+model_bact2 <- cor.test(as.numeric(DataTable_PhylaRelAbund_pH$BacterRelativeAbundance), as.numeric(DataTable_PhylaRelAbund_pH$pH),
+         method='spearman',
+         continuity = FALSE,
+         conf.level = 0.95)
+
+model_bact2
+
